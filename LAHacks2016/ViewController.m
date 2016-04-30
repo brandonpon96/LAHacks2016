e0467215f0fa813bd47a29638b3c04024ebba7b3
brandonpon96 <brandonpon96@gmail.com>
2016-04-30 12:53:21 -0700

    marvel api intergration

--- LAHacks2016/ViewController.m
+++ LAHacks2016/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
+#import "AFHTTPSessionManager.h"
+#import <CommonCrypto/CommonDigest.h>
 
 @interface ViewController ()
+
+@property (strong ,nonatomic) NSString *ts;
+@property (strong, nonatomic) NSString *code;
 
 @end
 
@@ -17,7 +23,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self findMale];
 }
+
+- (void)findMale{
+    
+    self.ts = [self timeStamp];
+    
+    self.code = [NSString stringWithFormat:@"%@08926590894eb0b868a5853b903e5c831b0fb2965770648b8171f0c6f10d5edb8e640b22",self.ts];
+    NSString* hash = [self convertIntoMD5:self.code];
+    NSString* request = [NSString stringWithFormat:@"http://gateway.marvel.com:80/v1/public/characters?name=Iron%%20man&ts=%@&apikey=5770648b8171f0c6f10d5edb8e640b22&hash=%@",self.ts,hash];
+    NSLog(@"%@",request);
+    NSURL *URL = [NSURL URLWithString:request];
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    [manager GET:URL.absoluteString parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
+        NSLog(@"JSON: %@", responseObject);
+    } failure:^(NSURLSessionTask *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+}
+
+- (NSString *) timeStamp {
+    return [NSString stringWithFormat:@"%f",[[NSDate date] timeIntervalSince1970] * 1000];
+}
+
+- (NSString *)convertIntoMD5:(NSString *) string{
+    const char *cStr = [string UTF8String];
+    unsigned char digest[16];
+    
+    CC_MD5( cStr, strlen(cStr), digest ); // This is the md5 call
+    
+    NSMutableString *resultString = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    
+    for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+        [resultString appendFormat:@"%02x", digest[i]];
+    return  resultString;
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
